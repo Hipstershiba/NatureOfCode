a2d8bb117bdfa968babe2ca643edc19f74410ee5
Hipstershiba <vitorrizzato@gmail.com>
2024-03-28 22:16:08 -0300

    chapter_0; exercise_04

---
+++ Chapter_0/exercise_04/Brush.js
@@ -0,0 +1,14 @@
+class Brush {
+    constructor(density, size) {
+        this.density = density;
+        this.size = size;
+    }
+
+    paint() {
+        for (let i = 0; i < this.density; i++) {
+            let x = randomGaussian(mouseX, this.size);
+            let y = randomGaussian(mouseY, this.size);
+            circle(x, y, map(dist(mouseX, mouseY, x, y), 0, this.size, 2, 0));
+        }
+    }
+}
--- Chapter_0/exercise_04/sketch.js
+++ Chapter_0/exercise_04/sketch.js
@@ -1,7 +1,21 @@
+let brush;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+  brush = new Brush(150, 5);
+  noStroke();
+  fill(0);
+  background(220);
 }
 
 function draw() {
-  background(220);
+  if (mouseIsPressed) {
+    brush.paint();
+  }
+}
+
+function keyPressed() {
+  if (key === 'r') {
+    background(220);
+  }
 }
